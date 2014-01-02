458efb3cf1cfe0f8d12035fe4591dbcca79c65dd
Jonathan Rahn <jonathan.rahn@hotmail.de>
2014-01-02 21:41:38 +0100

    Initial Commit

---
+++ SESpinEdit Demo/SEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SEAppDelegate.h
+//  SESpinEdit Demo
+//
+//  Created by Jonathan Rahn on 02.01.14.
+//  Copyright (c) 2014 Jonathan Rahn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SEAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SESpinEdit Demo/SEAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SEAppDelegate.m
+//  SESpinEdit Demo
+//
+//  Created by Jonathan Rahn on 02.01.14.
+//  Copyright (c) 2014 Jonathan Rahn. All rights reserved.
+//
+
+#import "SEAppDelegate.h"
+
+@implementation SEAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
