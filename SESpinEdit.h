46db25ef2eb936f15e05a25fd2a07e83794d83a9
Jonathan Rahn <jonathan.rahn@hotmail.de>
2014-01-02 23:54:13 +0100

    Interface documentated

--- SESpinEdit.h
+++ SESpinEdit.h
@@ -13,15 +13,31 @@
  # Minimal Width = 43
  */
 
+/**
+ This subclass of NSView provides the from other frameworks well-know SpinEdit. 
+ 
+ Technically the view contains a NSTextField and a NSStepper and keeps the both in sync, in addition, it validates
+ the input of the NSTextField.
+ 
+ @attention This view requires autolayout!
+ */
 @interface SESpinEdit : NSView<NSTextFieldDelegate>
 
+/**Provides the current value. When you set a value which is out of range, max/min will be setted.*/
 @property NSNumber* value;
+/**When activated, the SpinEdit accepts decimal values. When deactivated, the potential decimal value in value will be
+ cutted off to an integer.*/
 @property BOOL allowsFloats;
+/**The stepwidth of the stepper.*/
 @property NSNumber* increment;
+/**The lowest possible value. Default is -∞.*/
 @property NSNumber* min;
+/**The highest possible value. Default is +∞.*/
 @property NSNumber* max;
 
+/**Increases the value by the increment. Same effect like the up-button.*/
 - (void) increase;
+/**Decreases the value by the increment. Same effect like the down-button.*/
 - (void) decrease;
 
 @end
